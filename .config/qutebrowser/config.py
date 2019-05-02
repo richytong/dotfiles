c7d1093f3d568ea4fe7ecf55cf10f08c8d949dfc
Richard <richytong@gmail.com>
2019-05-02 13:54:43 -0700

    yt searchengine

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -2,5 +2,6 @@ c.tabs.position = "top"
 c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?hl=en&q={}",
     "ddg": "https://duckduckgo.com/?q={}",
-    "aw": "https://wiki.archlinux.org/?search={}"
+    "aw": "https://wiki.archlinux.org/?search={}",
+    "yt": "https://www.youtube.com/results?search_query={}"
 }
