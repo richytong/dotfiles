609ca9022ca64640ba78a1f644a5aa307dc56242
Richard <richytong@gmail.com>
2019-05-01 20:38:56 -0700

    qutebrowser searchengines

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,6 @@
+c.tabs.position = "top"
+c.url.searchengines = {
+    "DEFAULT": "https://www.google.com/search?hl=en&q={}",
+    "ddg": "https://duckduckgo.com/?q={}",
+    "aw": "https://wiki.archlinux.org/?search={}"
+}
