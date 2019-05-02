d9d86cb7348ff9db6ad32aa6dba59dc9800ccca0
Richard <richytong@gmail.com>
2019-05-02 16:38:44 -0700

    gh search

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,5 +3,6 @@ c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?hl=en&q={}",
     "ddg": "https://duckduckgo.com/?q={}",
     "aw": "https://wiki.archlinux.org/?search={}",
-    "yt": "https://www.youtube.com/results?search_query={}"
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "gh": "https://github.com/search?q={}"
 }
