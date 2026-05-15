877131408ac4ca7a825390a5e9a1060749b04587
Richard <richytong@gmail.com>
2026-05-15 09:25:07 -0700

    Revert "change default browser"
    This reverts commit deff80b69dcf91e0c7b947311dd13e3bef245f99.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,7 +3,7 @@ config.load_autoconfig()
 c.tabs.position = "top"
 
 c.url.searchengines = {
-    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "DEFAULT": "https://www.google.com/search?hl=en&q={}",
     "ddg": "https://duckduckgo.com/?q={}",
     "aw": "https://wiki.archlinux.org/?search={}",
     "yt": "https://www.youtube.com/results?search_query={}",
